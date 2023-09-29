fa823a3bf28a8871f62eee80572c3790ec3e6104
dalichabani <dalichabani@dalilaptop.home.org>
2023-09-29 14:03:49 +0100

    inserted libraries

--- server.c
+++ server.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define handle_error(msg) \
+   do {fprintf(stderr, "%s: %s\n", (msg), strerror(errno)); exit(EXIT_FAILURE); } while (0)
 
 int main()
 {
-    return 0;
+    return EXIT_SUCCESS;
 }
